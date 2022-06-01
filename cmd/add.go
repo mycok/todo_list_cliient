fd76948d53c61e4ec241f0dca7f4caa6f1d2bc0d
myko <awesomeme155@gmail.com>
2022-06-02 02:21:35 +0300

    add add command, command action and a client helper function

---
+++ cmd/add.go
@@ -0,0 +1,65 @@
+/*
+Copyright © 2022 mycok <github.com/mycok>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add <taskName>",
+	Short: "Add New Task",
+	SilenceUsage: true,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		rootURL := viper.GetString("api-root")
+
+		return addAction(os.Stdout, rootURL, args)
+	},
+}
+
+func addAction(w io.Writer, url string, args []string) error {
+	task := strings.Join(args, " ")
+
+	if err := addItem(url, task); err != nil {
+		return err
+	}
+
+	return printAddItem(w, task)
+}
+
+func printAddItem(w io.Writer, task string) error {
+	_, err := fmt.Fprintf(w, "Added task: %s : to the list\n", task)
+
+	return err
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
